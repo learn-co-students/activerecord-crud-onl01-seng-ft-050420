a97035f0fcf5f0e561f5714183b01699caea35d9
viankarivera <viankalrivera@gmail.com>
2020-06-10 23:49:25 -0400

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -64,13 +64,13 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  Movie.where("release_date > 2000").order(release_date: :desc)
+  Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
-  movie = Movie.create(title: "Awesome Flick")
-  movie = Movie.find_by(title: "Awesome FLick")
+  Movie.create(title: "Awesome Flick")
+  movie = Movie.find_by(title: "Awesome Flick")
   movie.update(title: "Even Awesomer Flick")
   movie.save
 end
